7d4e5b662200a248cae3b736cdeca2fcdb6461e3
Natali <piratika309@mail.ru>
2023-02-23 17:14:10 +0300

    миним сумма элементов строки

---
+++ example003/Program.cs
@@ -0,0 +1,39 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+int [,] array = new int [4,3];
+Random random = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = random.Next(0,10);
+        Console.Write($"{array[i,j]}\t");
+    }
+     Console.WriteLine();
+}
+Console.WriteLine();
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
